d4d6e619e96024d42b75ff4955ed9287176a1b32
ericbanker12 <ericbanker12@gmail.com>
2020-05-15 15:20:33 -0500

    test findById

--- src/test/java/demo/auth/services/UserServiceImplTest.java
+++ src/test/java/demo/auth/services/UserServiceImplTest.java
@@ -1,9 +1,14 @@
 package demo.auth.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.longThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,10 +39,37 @@ public class UserServiceImplTest {
             .thenReturn(Arrays.asList(new User("user", "pass")));
 
         List<User> users = userService.findAll();
-        
+
         assertEquals(1, users.size(), "user count is not 1");
         assertEquals("user", users.get(0).getUsername(), "username is not \"user\"");
         assertEquals("pass", users.get(0).getPassword(), "password is not \"pass\"");
+    }
+
+    @Test
+    void findByIdTest() {
+        User user = new User("user", "pass");
+        user.setId(1L);
+
+        Mockito
+            .when(userRepository.findById(longThat(id -> id == 1L)))
+            .thenReturn(Optional.of(user));
+
+        User testuser = userService.findById(1L);
+
+        assertEquals(1L, testuser.getId(), "user id is not 1");
+        assertEquals("user", testuser.getUsername(), "username is not \"user\"");
+        assertEquals("pass", testuser.getPassword(), "password is not \"pass\"");
+    }
+
+    @Test
+    void findById_BadIdThrowsExceptionTest() {
+        Optional<User> empty = Optional.empty();
+
+        Mockito
+            .when(userRepository.findById(anyLong()))
+            .thenReturn(empty);
+
+        assertThrows(NoSuchElementException.class, () -> userService.findById(1L));
     }
 
 }
